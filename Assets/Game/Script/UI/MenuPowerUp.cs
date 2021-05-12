19e0611244da713ad5ca7693b66d459a6793db5c
Viro <rousselotvianney@gmail.com>
2021-05-12 17:49:21 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Game/Script/UI/MenuPowerUp.cs
+++ Assets/Game/Script/UI/MenuPowerUp.cs
@@ -11,7 +11,9 @@ public class MenuPowerUp : MonoBehaviour
 
     public Image background;
 
-    public GameObject description;
+    public Text description;
+
+    [SerializeField] private int idPowerUp;
     
     // Start is called before the first frame update
     void Start()
@@ -22,12 +24,38 @@ public class MenuPowerUp : MonoBehaviour
     public void Select()
     {
         background.color = hoverColor;
-        description.SetActive(true);
+        UpdateDescription();
+        // description.SetActive(true);
     }
 
     public void Deselect()
     {
         background.color = baseColor;
-        description.SetActive(false);
+        // description.SetActive(false);
+    }
+
+    void UpdateDescription()
+    {
+        switch (idPowerUp)
+        {
+            case 1 :
+                description.text = "1";
+                break;
+            case 2 :
+                description.text = "2";
+                break;
+            case 3 :
+                description.text = "3";
+                break;
+            case 4 :
+                description.text = "4";
+                break;
+            case 5 :
+                description.text = "5";
+                break;
+            case 6 :
+                description.text = "6";
+                break;
+        }
     }
 }
