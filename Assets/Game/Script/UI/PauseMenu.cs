68f4dd7a71df0852c9dc029154e9926562df0f00
Viro <rousselotvianney@gmail.com>
2021-05-17 18:00:55 +0200

    Update UI + menu pause 1.0

---
+++ Assets/Game/Script/UI/PauseMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    // Panel "PauseMenu"
+    [SerializeField] private GameObject pauseMenuUI;
+    
+    // Player
+    [SerializeField] private PlayerController playerController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    // Resume
+    public void Resume()
+    {
+        playerController.controlCursor = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    // Pause
+    void Pause()
+    {
+        playerController.controlCursor = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+    public void QuitMenu()
+    {
+        Application.Quit();
+    }
+}
