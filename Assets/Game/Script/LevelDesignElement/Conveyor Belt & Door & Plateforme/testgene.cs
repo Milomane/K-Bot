0e1046cd68225b200a54a5f0061524ac7821747c
Goost <Paul.Snyk@gmail.com>
2021-05-26 14:33:30 +0200

    affinage plateforme

---
+++ Assets/Game/Script/LevelDesignElement/Conveyor Belt & Door & Plateforme/testgene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class testgene : MonoBehaviour
+{
+    public UnityEvent wallah;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
