2bce28c8cfdbff27ca8a7252e840d4bdbb66b8e8
Goost <Paul.Snyk@gmail.com>
2021-05-28 15:22:43 +0200

    le bool de Milo

---
+++ Assets/Game/Script/LevelDesignElement/C/ElectricalCapteurScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricalCapteurScript : MonoBehaviour
+{
+    public bool alimOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
