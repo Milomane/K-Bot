6f96bee68eb40ecbf31818ea37e9ba34690ccce0
Viro <rousselotvianney@gmail.com>
2021-05-31 11:51:41 +0200

    Cannon (in progress) + stop main music

--- Assets/Game/Script/LevelDesignElement/FactoryCannon.cs
+++ Assets/Game/Script/LevelDesignElement/FactoryCannon.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class FactoryCannon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Projectile compartment
+    [SerializeField] private Transform projectileCompartment;
+    
+    // Fire power of the cannon
+    [SerializeField] private float firePower = 2f;
+    
+    // Corpse
+    private GameObject corpseKbot;
+    
+    // Is the cannon loaded
+    private bool isCannonLoaded;
 
-    // Update is called once per frame
-    void Update()
+    public void Fire()
     {
-        
+        if (isCannonLoaded)
+        {
+            // Fire the corpseKbot
+            Debug.Log("Fire !!!");
+            corpseKbot.GetComponent<Rigidbody>().isKinematic = false;
+            corpseKbot.GetComponent<Rigidbody>().velocity = firePower * corpseKbot.transform.forward;
+            isCannonLoaded = false;
+            // Play VFX (smoke, explosion, ...)
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Corpse") && !isCannonLoaded)
+        {
+            corpseKbot = other.gameObject;
+            corpseKbot.transform.rotation = transform.rotation;
+            
+            // Cannon loading
+            corpseKbot.transform.position = projectileCompartment.position;
+            
+            // Cannon is loaded
+            isCannonLoaded = true;
+            
+            // Bloc the corpse
+            corpseKbot.GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 }
