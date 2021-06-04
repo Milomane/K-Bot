2589305024731de8b6236fc5d687de0f56c4f209
drimnas <clovismichaud1287@gmail.com>
2021-06-04 17:11:21 +0200

    particule lava + acid

--- Assets/Game/Script/LevelDesignElement/Acid.cs
+++ Assets/Game/Script/LevelDesignElement/Acid.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class Acid : MonoBehaviour
 {
-    public GameObject player;
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // destroy the player
@@ -17,7 +15,7 @@ public class Acid : MonoBehaviour
         if (other.CompareTag("Corpse")) // destroy the player body
         {
             Transform test = other.gameObject.transform.parent;
-            player.GetComponent<PlayerDeathHandler>().DestroySelectedBody(test.gameObject);
+            PlayerDeathHandler.instance.DestroySelectedBody(test.gameObject);
         }
     }
 }
