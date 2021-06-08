1aa442e0d20ef4d2b94f6ada24a4f857c9199bc4
drimnas <clovismichaud1287@gmail.com>
2021-06-08 14:43:22 +0200

    begin crunched particle

--- Assets/Game/Script/Player/PlayerDeathHandler.cs
+++ Assets/Game/Script/Player/PlayerDeathHandler.cs
@@ -116,7 +116,7 @@ public class PlayerDeathHandler : MonoBehaviour
         if (deaths[(int)deathType].eventPrefab != null)
             eventObject = Instantiate(deaths[(int)deathType].eventPrefab, transform.position, transform.rotation);
         if (deaths[(int)deathType].particlePrefab != null)
-            particleObject = Instantiate(deaths[(int)deathType].particlePrefab, transform.position, transform.rotation);
+            particleObject = Instantiate(deaths[(int)deathType].particlePrefab, transform.position, quaternion.Euler(-90,0,0));
 
         float maxRbAngular = 1;
         
