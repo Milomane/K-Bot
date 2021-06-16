c8de277895f743dd9d822daa46059f91b99b7aa6
Florentin <florentin.roberge@gmail.com>
2021-06-16 17:01:56 +0200

    Modif wall material

--- Assets/Game/Script/PowerUp/GeneratorSphereRange.cs
+++ Assets/Game/Script/PowerUp/GeneratorSphereRange.cs
@@ -1,15 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GeneratorSphereRange : MonoBehaviour
 {
+    public List<ElectricalCapteurScript> allCaptorInRange;
+    
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "ElectricalCapteur")
         {
             Debug.Log("Marche");
             other.GetComponent<ElectricalCapteurScript>().alimOn = true;
+            allCaptorInRange.Add(other.GetComponent<ElectricalCapteurScript>());
         }
     }
     
@@ -19,6 +23,18 @@ public class GeneratorSphereRange : MonoBehaviour
         {
             Debug.Log("Marche mais a l'envers");
             other.GetComponent<ElectricalCapteurScript>().alimOn = false;
+            if (allCaptorInRange.Contains(other.GetComponent<ElectricalCapteurScript>()))
+            {
+                allCaptorInRange.Remove(other.GetComponent<ElectricalCapteurScript>());
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var captor in allCaptorInRange)
+        {
+            captor.alimOn = false;
         }
     }
 }
