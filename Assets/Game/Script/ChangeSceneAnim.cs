d540a39fbb95584649708fce33d26785d2da8b25
Florentin <florentin.roberge@gmail.com>
2021-06-27 23:39:59 +0200

    Cutscene work

--- Assets/Game/Script/ChangeSceneAnim.cs
+++ Assets/Game/Script/ChangeSceneAnim.cs
@@ -7,15 +7,20 @@ using UnityEngine.Video;
 public class ChangeSceneAnim : MonoBehaviour
 {
     public VideoPlayer vid;
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+        StartCoroutine(WaitEndToChangeScene());
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-       Debug.Log("play");
+    }
+
+    public IEnumerator WaitEndToChangeScene()
+    {
+        yield return new WaitForSeconds(68f);
+        SceneManager.LoadScene("HubIntro");
     }
 }
