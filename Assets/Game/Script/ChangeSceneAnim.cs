6412905a90e4f545a0eca39371e2fd5ae46ca153
Florentin <florentin.roberge@gmail.com>
2021-06-28 11:53:23 +0200

    TEst

--- Assets/Game/Script/ChangeSceneAnim.cs
+++ Assets/Game/Script/ChangeSceneAnim.cs
@@ -6,7 +6,13 @@ using UnityEngine.Video;
 
 public class ChangeSceneAnim : MonoBehaviour
 {
+    public float time = 68f;
+    public string sceneToLoad = "HubIntro";
+    
     public VideoPlayer vid;
+
+    public bool check;
+    public GameObject skipText;
     
     void Start()
     {
@@ -16,11 +22,31 @@ public class ChangeSceneAnim : MonoBehaviour
     
     void Update()
     {
+        if (Input.GetButtonDown("Submit") || Input.GetButtonDown("Jump"))
+        {
+            if (!check)
+            {
+                StartCoroutine(CheckWait());
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
     }
 
     public IEnumerator WaitEndToChangeScene()
     {
-        yield return new WaitForSeconds(68f);
-        SceneManager.LoadScene("HubIntro");
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public IEnumerator CheckWait()
+    {
+        check = true;
+        skipText.SetActive(true);
+        yield return new WaitForSeconds(5);
+        check = false;
+        skipText.SetActive(false);
     }
 }
